46ea8ba013b3b2f524cae0944f89c68187b6a192
Jonathan <johjo@ytreza.org>
2025-07-25 10:41:24 +0200

    feat: list task from todolist (wip)

---
+++ src/ytreza_dev/features/todolist_query.py
@@ -0,0 +1,20 @@
+from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
+
+@dataclass
+class Task:
+    name: str
+
+
+class TodolistReaderPort(metaclass=ABCMeta):
+    @abstractmethod
+    def all_tasks(self) -> list[Task]:
+        pass
+
+
+class TodolistQuery:
+    def __init__(self, todolist_reader: TodolistReaderPort):
+        self._todolist_reader = todolist_reader
+
+    def all_tasks(self) -> list[Task]:
+        return self._todolist_reader.all_tasks()
---
+++ tests/features/todolist_query/test_list_all_tasks_from_todolist.py
@@ -0,0 +1,36 @@
+import pytest
+
+from ytreza_dev.features.todolist_query import Task, TodolistReaderPort, TodolistQuery
+
+
+class TodolistReaderForTest(TodolistReaderPort):
+    def __init__(self) -> None:
+        self._all_tasks : list[Task] = []
+
+    def all_tasks(self) -> list[Task]:
+        return self._all_tasks
+
+    def feed(self, tasks: list[Task]) -> None:
+        self._all_tasks = tasks
+
+
+@pytest.fixture
+def todolist_reader() -> TodolistReaderForTest:
+    return TodolistReaderForTest()
+
+@pytest.fixture
+def sut(todolist_reader: TodolistReaderForTest) -> TodolistQuery:
+    return TodolistQuery(todolist_reader)
+
+
+def test_list_nothing_when_no_task(sut: TodolistQuery) -> None:
+    assert sut.all_tasks() == []
+
+
+def test_list_tasks(sut: TodolistQuery, todolist_reader: TodolistReaderForTest) -> None:
+    todolist_reader.feed([Task(name="buy the milk"), Task(name="buy the water")])
+
+    all_tasks = sut.all_tasks()
+
+    assert all_tasks == [Task(name="buy the milk"), Task(name="buy the water")]
+
