682e2107264652a2ddf429f8fc492d4012da92c3
Jonathan <johjo@ytreza.org>
2025-08-08 11:58:07 +0200

    feat: remove choose_task [BREAKING CHANGE]

--- src/ytreza_dev/features/final_version_perfected/controller.py
+++ src/ytreza_dev/features/final_version_perfected/controller.py
@@ -2,7 +2,6 @@ from pyqure import PyqureMemory, pyqure # type: ignore
 
 from ytreza_dev.features.final_version_perfected.use_case.do_never import DoNever
 from ytreza_dev.features.final_version_perfected.use_case.do_next import DoNext
-from ytreza_dev.features.final_version_perfected.use_case.choose_task import ChooseTaskUseCase
 from ytreza_dev.features.final_version_perfected.injection_keys import TODOLIST_READER_KEY, TASK_REPOSITORY_KEY, \
     TASK_READER_KEY, EXTERNAL_TODOLIST_KEY
 from ytreza_dev.features.final_version_perfected.use_case.close_task_use_case import CloseTaskUseCase
@@ -24,9 +23,6 @@ class FvpController:
 
     def next_action(self) -> NextAction :
         return NextActionFvpQuery(task_reader=self._inject(TASK_READER_KEY)).next_action()
-
-    def choose_task(self, url: str) -> None:
-        ChooseTaskUseCase(task_repository=self._inject(TASK_REPOSITORY_KEY)).execute(url)
 
     def close_task(self, url: str) -> None:
         CloseTaskUseCase(task_repository=self._inject(TASK_REPOSITORY_KEY), external_todolist=self._inject(EXTERNAL_TODOLIST_KEY)).execute(url)
